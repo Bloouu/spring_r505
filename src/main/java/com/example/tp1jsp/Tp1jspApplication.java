a5b88821bd573e4ffb0e1e6c5dafae0eb43eea31
Beurisseuh <brice.monkonkala@etu.iut-tlse3.fr>
2024-11-21 15:04:02 +0100

    Initial commit

---
+++ src/main/java/com/example/tp1jsp/Tp1jspApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp1jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1jspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp1jspApplication.class, args);
+    }
+
+}
